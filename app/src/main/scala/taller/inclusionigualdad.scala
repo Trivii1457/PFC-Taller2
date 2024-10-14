c5f552bfc0aceef29525daaf516730bac9c9d9b9
Trivii1457 <trivi1457@gmail.com>
2024-10-14 11:03:17 -0500

    Se agrega correccion en la clase IncIgual

--- app/src/main/scala/taller/inclusionigualdad.scala
+++ app/src/main/scala/taller/inclusionigualdad.scala
@@ -2,18 +2,21 @@ package taller
 
 class inclusionigualdad {
   
+    type ConjuntoDifuso = Int => Double
+
+
     // Función auxiliar para verificar inclusión con recursión de cola
-def inclusionAux(cd1: ConjDifuso, cd2: ConjDifuso, elem: Int): Boolean = {
+    def inclusionAux(cd1: ConjuntoDifuso, cd2: ConjuntoDifuso, elem: Int): Boolean = {
 
-    if (elem > 1000) true
-    else if (cd1(elem) > cd2(elem)) false
-    else inclusionAux(cd1, cd2, elem + 1)
-}
+        if (elem > 1000) true
+        else if (cd1(elem) > cd2(elem)) false
+        else inclusionAux(cd1, cd2, elem + 1)
+    }
 
-// Función principal de inclusión
-def inclusion(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = {
-    inclusionAux(cd1, cd2, 0)  // Comenzamos a iterar desde el elemento 0
-}
+    // Función principal de inclusión
+    def inclusion(cd1: ConjuntoDifuso, cd2: ConjuntoDifuso): Boolean = {
+        inclusionAux(cd1, cd2, 0)  // Comenzamos a iterar desde el elemento 0
+    }
 
 
 
