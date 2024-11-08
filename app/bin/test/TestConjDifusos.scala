8afe6c9fa5767761f8a3cc9816a4a61fa94c74db
Trivii1457 <trivi1457@gmail.com>
2024-11-07 19:20:59 -0500

    Fix Test

--- app/bin/test/TestConjDifusos.scala
+++ app/bin/test/TestConjDifusos.scala
@@ -34,19 +34,19 @@ class TestConjDifusos extends  AnyFunSuite{
 
     // Test para la función Complemento
     test("Complemento con valor normal 1") {
-        assert(objConjDifusos.complemento(objConjDifusos.grande(1, 5))(3) == 0.5)
+        assert(objConjDifusos.complemento(objConjDifusos.grande(1, 5))(3) == 0.763)
     }
 
     test("Complemento con valor normal 2") {
-        assert(objConjDifusos.complemento(objConjDifusos.grande(1, 5))(4) == 0.25)
+        assert(objConjDifusos.complemento(objConjDifusos.grande(1, 7))(4) == 0.79)
     }
 
     test("Complemento con valor grande 1") {
-        assert(objConjDifusos.complemento(objConjDifusos.grande(1, 1000))(500) == 0.5)
+        assert(objConjDifusos.complemento(objConjDifusos.grande(1, 1000))(500) == 0.864)
     }
 
     test("Complemento con valor grande 2") {
-        assert(objConjDifusos.complemento(objConjDifusos.grande(1, 1000))(999) == 0.001)
+        assert(objConjDifusos.complemento(objConjDifusos.grande(1, 1000))(999) == 0.632)
     }
 
     test("Complemento con valor fuera de rango") {
@@ -57,19 +57,19 @@ class TestConjDifusos extends  AnyFunSuite{
 
     // Test para la función Union
     test("Union con valor normal 1") {
-        assert(objConjDifusos.Union(objConjDifusos.grande(1, 5), objConjDifusos.muchoMayorQue(3, 7))(3) == 0.5)
+        assert(objConjDifusos.Union(objConjDifusos.grande(1, 5), objConjDifusos.muchoMayorQue(3, 7))(3) == 0.237)
     }
 
     test("Union con valor normal 2") {
-        assert(objConjDifusos.Union(objConjDifusos.grande(1, 5), objConjDifusos.muchoMayorQue(3, 7))(4) == 0.75)
+        assert(objConjDifusos.Union(objConjDifusos.grande(1, 5), objConjDifusos.muchoMayorQue(3, 7))(4) == 0.328)
     }
 
     test("Union con valor grande 1") {
-        assert(objConjDifusos.Union(objConjDifusos.grande(1, 1000), objConjDifusos.muchoMayorQue(500, 1000))(500) == 0.5)
+        assert(objConjDifusos.Union(objConjDifusos.grande(1, 1000), objConjDifusos.muchoMayorQue(500, 1000))(500) == 0.136)
     }
 
     test("Union con valor grande 2") {
-        assert(objConjDifusos.Union(objConjDifusos.grande(1, 1000), objConjDifusos.muchoMayorQue(500, 1000))(999) == 0.999)
+        assert(objConjDifusos.Union(objConjDifusos.grande(1, 1000), objConjDifusos.muchoMayorQue(500, 1000))(999) == 0.998)
     }
 
     test("Union con valor fuera de rango") {
